97bc0ddc8f9d478389fb42652a992d8db9c26555
= <resitylmz@gmail.com>
2020-01-15 14:05:17 +0300

    tasarımda urunler sayfasının entegrasi

---
+++ ETİCARET/ETİCARET.UI/Controllers/UrunlerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETİCARET.UI.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        // GET: Urunler
+        public ActionResult Index()
+        {
+            return View("Urunler");
+        }
+        public ActionResult UrunDetay()
+        {
+            return View("UrunDetay");
+        }
+    }
+}
